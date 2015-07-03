28a3d906c74e641160691a4a60afa7c46721f944
susana-angulo <susana_angulo@yahoo.com>
2015-07-03 18:11:00 +0200

    Revert "random versión 2"
    This reverts commit c7e57ffe977a1df2b9b5c9f19801a701588be307.

--- random.js
+++ random.js
@@ -1,10 +1,9 @@
 // Math.random() devuelve número aleatorio entre 0 y 1.
 var numero = Math.random();
 
-var str =' MAYOR que 0,5';
-
 if (numero <= 0.5){
-  str = ' MENOR que 0,5';
+  console.log('\n' + numero + ' MENOR que 0,5 \n');
 }
-
-console.log('\n' + numero + str + '\n');
+else {
+  console.log('\n' + numero + ' MAYOR que 0,5 \n');
+}
